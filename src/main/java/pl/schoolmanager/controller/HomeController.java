0727e221b9006a5ad7db6f50b401e602d20787b6
Lukasz.Budyn <lukasz-budyn@wp.pl>
2018-01-25 11:21:48 +0100

    db

--- src/main/java/pl/schoolmanager/controller/HomeController.java
+++ src/main/java/pl/schoolmanager/controller/HomeController.java
@@ -1,12 +1,30 @@
 package pl.schoolmanager.controller;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller("/")
 public class HomeController {
 
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+
+	@Autowired
+	private DataSource dataSource;
+
+	
 	@GetMapping("/")
 	public String home() {
 		return "home/home";
@@ -26,5 +44,28 @@ public class HomeController {
 	public String accessDenied() {
 		return "errors/accessDenied";
 	}
+	
+	
+	@RequestMapping("/db")
+	String db(Map<String, Object> model) {
+		try (Connection connection = dataSource.getConnection()) {
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+			stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+			ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+			ArrayList<String> output = new ArrayList<String>();
+			while (rs.next()) {
+				output.add("Read from DB: " + rs.getTimestamp("tick"));
+			}
+
+			model.put("records", output);
+			return "db";
+		} catch (Exception e) {
+			model.put("message", e.getMessage());
+			return "error";
+		}
+	}
+
 
 }
---
+++ src/main/webapp/WEB-INF/views/home/error.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8" isELIgnored="false"%>
+<%@include file="../jspf/includes.jspf"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<%@ include file="../jspf/headconfig.jspf"%>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Homepage</title>
+</head>
+<body>
+
+	<%@ include file="../jspf/home_menu.jspf"%>
+
+	<div class="jumbotron">
+
+		<h1>
+			DB view - a jednak blad!</b>.
+		</h1>
+	</div>
+
+
+	<%@ include file="../jspf/footer.jspf"%>
+</body>
+</html>
