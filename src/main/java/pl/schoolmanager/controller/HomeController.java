c75a87833b0bb67fc5d24b98e72adfdd1bc04196
Lukasz.Budyn <lukasz-budyn@wp.pl>
2018-01-25 12:21:13 +0100

    db

--- src/main/java/pl/schoolmanager/controller/HomeController.java
+++ src/main/java/pl/schoolmanager/controller/HomeController.java
@@ -56,7 +56,7 @@ public class HomeController {
 			stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
 			stmt.executeUpdate("INSERT INTO names VALUES ('Lukasz')");
 			ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-			ResultSet rs2 = stmt.executeQuery("SELECT name FROM names");
+//			ResultSet rs2 = stmt.executeQuery("SELECT name FROM names");
 
 			ArrayList<String> output = new ArrayList<String>();
 			while (rs.next()) {
@@ -64,17 +64,17 @@ public class HomeController {
 				System.out.println(rs.getTimestamp("tick"));
 			}
 
-			ArrayList<String> output2 = new ArrayList<String>();
-			while (rs2.next()) {
-				output2.add("Read from DB: " + rs2.getString("name"));
-				System.out.println(rs.getString("name"));
-			}
+//			ArrayList<String> output2 = new ArrayList<String>();
+//			while (rs2.next()) {
+//				output2.add("Read from DB: " + rs2.getString("name"));
+//				System.out.println(rs.getString("name"));
+//			}
 
 			
 			model.put("ticks", output);
-			model.put("names", output2);
+//			model.put("names", output2);
 			m.addAttribute("ticks", output);
-			m.addAttribute("names", output2);
+//			m.addAttribute("names", output2);
 			
 			return "home/db";
 		} catch (Exception e) {
