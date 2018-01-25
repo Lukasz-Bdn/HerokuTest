c500a5fcd8537e25ccdc10e37e262aff0221325d
Lukasz.Budyn <lukasz-budyn@wp.pl>
2018-01-25 13:25:12 +0100

    db

--- src/main/java/pl/schoolmanager/app/AppConfig.java
+++ src/main/java/pl/schoolmanager/app/AppConfig.java
@@ -36,20 +36,6 @@ import com.zaxxer.hikari.HikariDataSource;
 // @Import({ SecurityConfig.class })
 public class AppConfig extends WebMvcConfigurerAdapter {
 	
-//    @Bean
-//    public BasicDataSource dataSource() throws URISyntaxException {
-//        String dbUrl = System.getenv("JDBC_DATABASE_URL");
-//        String username = System.getenv("JDBC_DATABASE_USERNAME");
-//        String password = System.getenv("JDBC_DATABASE_PASSWORD");
-//
-//        BasicDataSource basicDataSource = new BasicDataSource();
-//        basicDataSource.setUrl(dbUrl);
-//        basicDataSource.setUsername(username);
-//        basicDataSource.setPassword(password);
-//
-//        return basicDataSource;
-//    }
-
 	@Value("${spring.datasource.url}")
 	private String dbUrl;
 	
