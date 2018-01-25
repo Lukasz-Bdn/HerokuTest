593bc2747f9ed290d863ece8dda392df967b31f2
Lukasz.Budyn <lukasz-budyn@wp.pl>
2018-01-25 12:00:59 +0100

    db

--- src/main/java/pl/schoolmanager/controller/HomeController.java
+++ src/main/java/pl/schoolmanager/controller/HomeController.java
@@ -57,7 +57,9 @@ public class HomeController {
 		try (Connection connection = dataSource.getConnection()) {
 			Statement stmt = connection.createStatement();
 			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS names (name)");
 			stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+			stmt.executeUpdate("INSERT INTO names VALUES ('Lukasz')");
 			ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
 
 			ArrayList<String> output = new ArrayList<String>();
